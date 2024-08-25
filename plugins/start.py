3e0a884a66c6c781db5f7f2b030c817847e248d7
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2024-08-26 00:22:26 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
+                    InlineKeyboardButton("⚡️ Aʙᴏᴜᴛ", callback_data = "about"),
+                    InlineKeyboardButton('🍁 HᴇᴀᴠᴇɴʟʏSᴜʙs', url='https://t.me/BattleThroughTheHeavenSubs')
                 ]
             ]
         )
